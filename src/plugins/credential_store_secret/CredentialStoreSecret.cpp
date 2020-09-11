1285c1aea0c21e0e497cfa3faad06ebadf4c6edb
James Buren <braewoods+mgh@braewoods.net>
2020-09-11 02:42:28 -0500

    wee

--- src/plugins/credential_store_secret/CredentialStoreSecret.cpp
+++ src/plugins/credential_store_secret/CredentialStoreSecret.cpp
@@ -1,2 +1,14 @@
 #include <libsecret/secret.h>
 #include "./CredentialStoreSecret.h"
+
+// Schema used to store WebCredentials
+static const SecretSchema Schema = {
+    CredentialStore_iid,
+    SECRET_SCHEMA_NONE,
+    {
+        { "host",      SECRET_SCHEMA_ATTRIBUTE_STRING,  },
+        { "lastlogin", SECRET_SCHEMA_ATTRIBUTE_INTEGER, },
+        { "username",  SECRET_SCHEMA_ATTRIBUTE_STRING,  },
+        { "formdata",  SECRET_SCHEMA_ATTRIBUTE_STRING,  },
+    },
+};
