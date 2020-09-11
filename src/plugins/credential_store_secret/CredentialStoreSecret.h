62813e76adaf1b09b71c77b4dbf5d178b95f2d5c
James Buren <braewoods+mgh@braewoods.net>
2020-09-11 00:10:52 -0500

    wee

--- src/plugins/credential_store_secret/CredentialStoreSecret.h
+++ src/plugins/credential_store_secret/CredentialStoreSecret.h
@@ -3,4 +3,12 @@
 
 #include "CredentialStore.h"
 
+/**
+ * @class CredentialStoreSecret
+ * @brief Implementation of the credential store using Secret as a backend storage system
+ */
+class CredentialStoreSecret : public QObject, public CredentialStore
+{
+};
+
 #endif // CREDENTIALSTORESECRET_H
